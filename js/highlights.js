0b6b1292d99959743657a6b2f1f015fd94a79dce
rashikapr <74289013+rashikapr@users.noreply.github.com>
2024-02-27 11:47:48 +0530

    Update highlights.js

--- js/highlights.js
+++ js/highlights.js
@@ -8,35 +8,11 @@ AFRAME.registerComponent("cursor-listener", {
   },  
   handleMouseEnterEvents: function () {
     // Mouse Enter Events
-    this.el.addEventListener("mouseenter", () => {
-      const id = this.el.getAttribute("id");
-      const postersId = [
-        "superman",
-        "spiderman",
-        "captain-aero",
-        "outer-space",
-      ];
-      if (postersId.includes(id)) {
-        const postersContainer = document.querySelector("#posters-container");
-        postersContainer.setAttribute("cursor-listener", {
-          selectedItemId: id,
-        });
-        this.el.setAttribute("material", { color: "#1565c0" });
-      }
-    });
+    
   },
   handleMouseLeaveEvents: function () {
     // Mouse Leave Events
-    this.el.addEventListener("mouseleave", () => {
-      const { selectedItemId } = this.data;
-      if (selectedItemId) {
-        const el = document.querySelector(`#${selectedItemId}`);
-        const id = el.getAttribute("id");
-        if (id == selectedItemId) {
-          el.setAttribute("material", { color: "#fff" });
-        }
-      }
-    });
+    
   },
   
 });
